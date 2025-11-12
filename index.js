6ae12f897f44eff133153aeed9e7cb9ee75d9efe
long <sakakiyuya.arc.v.25@gmail.com>
2025-11-12 11:04:48 +0700

    fixbug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const route = require("./routes/client/index-route")
 const routeAdmin = require("./routes/admin/index-route")
 const systemConfig = require("./config/system");
-const boddyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -15,13 +15,23 @@ const app = express();
 app.use(methodOverride("_method"));
 const port = process.env.PORT;
 
-app.use(boddyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 app.use(cookieParser('secret'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(session({ 
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    maxAge: 60000,
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true
+  }
+}));
 app.use(flash());
 
 app.use((req, res, next) => {
